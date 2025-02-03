06760c7e397de4353c5ff42278bca2c4058c0b1f
mthsandrioli <matheus_andrioli@hotmail.com>
2025-02-03 12:27:00 -0300

    feat: create json database and perist data

--- src/server.js
+++ src/server.js
@@ -1,31 +1,35 @@
-import http from "node:http";
+import http from 'node:http'
+import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
-const users = [];
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
+  const { method, url } = req
 
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .end(JSON.stringify(users));
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
-    
-    users.push({
+
+    const user = {
       id: 1,
       name,
-      email
-    })
-    
-    return res.writeHead(201).end();
+      email,
+    }
+
+    database.insert('users', user)
+
+    return res.writeHead(201).end()
   }
 
-  return res.writeHead(404).end();
-});
+  return res.writeHead(404).end()
+})
 
-server.listen(3333);
+server.listen(3333)
